5213a985cddba604a19d9cbf88cf00865860ae9c
jlind0 <lind@yahoo.com>
2023-08-21 09:04:09 -0500

    undid dispatcher, added logging

--- Bobaloo.Hangman.Client/MainPage.xaml.cs
+++ Bobaloo.Hangman.Client/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Bobaloo.Hangman.Client.Dispatcher;
 using Bobaloo.Hangman.ViewModels;
+using ReactiveUI;
 using ReactiveUI.Maui;
+using System.Reactive.Disposables;
 
 namespace Bobaloo.Hangman.Client
 {
@@ -10,6 +12,14 @@ namespace Bobaloo.Hangman.Client
         public MainPage(MainWindowViewModel vm)
         {
             ViewModel = vm;
+            this.WhenActivated(d =>
+            {
+                ViewModel.Alert.RegisterHandler(async interaction =>
+                {
+                    await DisplayAlert("Alert", interaction.Input, "OK");
+                    interaction.SetOutput(true);
+                }).DisposeWith(d);
+            });
             vm.DispatcherService = new MauiDispatcher(Dispatcher);
             InitializeComponent();
             BindingContext = ViewModel;
--- Bobaloo.Hangman.ViewModels/MainWindowViewModel.cs
+++ Bobaloo.Hangman.ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Client;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,8 @@ namespace Bobaloo.Hangman.ViewModels
 {
     public class MainWindowViewModel : ReactiveObject
     {
+        private readonly Interaction<string, bool> alert;
+        public Interaction<string, bool> Alert => alert;
         public IDispatcherService DispatcherService { get; set; } = null!;
         protected IPublicClientApplication ClientApplication { get; }
         private bool isLoggedIn = false;
@@ -23,19 +27,29 @@ namespace Bobaloo.Hangman.ViewModels
         }
         public ICommand Login { get; }
         protected string ApiScope { get; }
-        public MainWindowViewModel(IConfiguration config, IPublicClientApplication clientApplication)
+        protected ILogger Logger { get; }
+        public MainWindowViewModel(IConfiguration config, IPublicClientApplication clientApplication, ILogger<MainWindowViewModel> logger)
         {
+            alert = new Interaction<string, bool>();
             ClientApplication = clientApplication;
+            Logger = logger;
             Login = ReactiveCommand.CreateFromTask(DoLogin);
             ApiScope = config["MicrosoftGraph:Scopes"] ?? throw new InvalidDataException();
         }
         public async Task DoLogin(CancellationToken token = default)
         {
-            
-            var result = await ClientApplication.AcquireTokenInteractive(new string[] { ApiScope })
-                        .WithPrompt(Prompt.SelectAccount).ExecuteAsync(token)
-                        .ConfigureAwait(false);
-            await DispatcherService.Dispatch(() => IsLoggedIn = true);
+            try
+            {
+                var result = await ClientApplication.AcquireTokenInteractive(new string[] { ApiScope })
+                            .WithPrompt(Prompt.SelectAccount).ExecuteAsync(token)
+                            .ConfigureAwait(false);
+                IsLoggedIn = true;
+            }
+            catch(Exception ex)
+            {
+                await Alert.Handle(ex.StackTrace ?? "No stack trace").GetAwaiter();
+                Logger.LogError(ex, ex.Message);
+            }
         }
     }
 }
